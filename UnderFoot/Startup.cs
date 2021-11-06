0a953a7bab6485ad9d2c7d5c32a930ecc448981c
ckconur <cekiconurr@gmail.com>
2021-11-06 13:20:27 +0300

    Files structure, connection string

--- UnderFoot/Startup.cs
+++ UnderFoot/Startup.cs
@@ -1,24 +1,37 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UnderFoot.Models.DataAccess;
 
 namespace UnderFoot
 {
     public class Startup
     {
+        public IConfiguration configuration { get; } 
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UnderFootDBContext>(options =>
+            {
+                options.UseSqlServer(configuration["ConnectionStrings:UnderFootConnString"]);
+            });
             services.AddControllersWithViews();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
